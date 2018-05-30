61e3fca3ae51e2eb483c9ce0c36f2b41573ca4f7
Melody Morse <mermaidofhyrule@gmail.com>
2018-05-30 13:14:11 -0400

    bot posts hello world now

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello World");
+
+var Twitter = require('twitter');
+var config = require('./config');
+
+var T = new Twitter({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token_key: config.access_token_key,
+    access_token_secret: config.access_token_secret
+});
+var tweet = {
+    status: 'Hello Girls'
+}
+T.post('statuses/update', tweet, callback );
+
+function callback(error, data, response) {
+    if(error) console.log(error);
+    else console.log("posted!")
+    //    console.log(data);  // Tweet body.
+    //console.log(response);  // Raw response object. 
+    }

b09307e241909cf7505d523cb18d759fd5194ed2
Melody Morse <mermaidofhyrule@gmail.com>
2018-05-31 08:54:46 -0400

    hello world function updated to not get err for repeat post

--- index.js
+++ index.js
@@ -9,14 +9,19 @@ var T = new Twitter({
     access_token_key: config.access_token_key,
     access_token_secret: config.access_token_secret
 });
-var tweet = {
-    status: 'Hello Girls'
-}
-T.post('statuses/update', tweet, callback );
-
-function callback(error, data, response) {
-    if(error) console.log(error);
-    else console.log("posted!")
-    //    console.log(data);  // Tweet body.
-    //console.log(response);  // Raw response object. 
+//postTweet(); uncomment to test tweet
+function postTweet()
+{
+    var n = Math.floor(Math.random() * 100);
+    var tweet = {
+        status: 'Hello World ' + n
     }
+    T.post('statuses/update', tweet, callback );
+
+    function callback(error, data, response) {
+        if(error) console.log(error);
+        else console.log("posted!")
+        //    console.log(data);  // Tweet body.
+        //console.log(response);  // Raw response object. 
+        }
+}
